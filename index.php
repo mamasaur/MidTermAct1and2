f5be5925ed1d7e52aa816061787300e91865a39d
hermoine <vehllej@gmail.com>
2025-09-03 22:25:34 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Document </title>
+    <title> ACT1&&ACT2</title>
+</head>
+<body>
+    <?php
+
+        include 'Act2.php';
+        include 'Act1.php';
+
+
+    ?>
